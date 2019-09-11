131247aacede7e39918bb8709907bda62b8c3842
anandstha <anand.stha.dev@gmail.com>
2019-09-11 16:00:42 +0545

    status

--- app/Http/Controllers/frontend/Shop/FacebookController.php
+++ app/Http/Controllers/frontend/Shop/FacebookController.php
@@ -34,21 +34,18 @@ class FacebookController extends Controller
             $createName = $user->getName();
             $createFacebook_id = $user->getId();
             $createEmail = $user->getEmail() ?? $user->getId() . '@fb.com';
-
             $user = User::where('email', $createEmail)->first();
-            if (!$user)
+            if (!$user){
                 $user = User::create([
                     'name' => $createName,
                     'surname' => $createName,
                     'user_name' => $createFacebook_id,
                     'email' => $createEmail,
-                    'password' => bcrypt($createFacebook_id),
                     'country_id' => 1,
                     'provider'=>'facebook',
                     'provider_id'=>$createFacebook_id
                 ]);
-
-
+            }
             auth()->login($user);
             return redirect()->to('/');
         } catch (Exception $e) {
